535469e061f15f4072f928206a7ae63c13a9dd1b
Danil <homie.dont.kill.my.vibe@gmail.com>
2016-12-23 13:59:38 +0300

    add script

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.menu__ad').click(function(){
+    $('.nav').slideToggle(500);
+    $('menu__show').toggleClass("active");
+});
+$(window).resize(function(){
+ if($(window).width() > 700){
+    $('.nav').removeAttr('style') 
+};
+
+});
+});
